da1819cec14816dbed547b5678ee4760767f0c25
sk1700 <112384@emmauscollege.nl>
2021-11-05 09:29:16 +0000

    gitaar ingevoegd

--- db/create.sql
+++ db/create.sql
@@ -57,7 +57,9 @@ insert into products (id, name, description, kleuren_product_id, merk_id, catego
 (3, 'Scarlet and the Black, The', 'Pellentesque at nulla. ', 1, 1, 1, 13.5);
 
 insert into products (id, name, description, kleuren_product_id, merk_id, categorie_id, price) values 
-(4, 'Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, ', 1, 1, 1, 13.5);
+(4, 'Fender Classic Design CD-140SCE Natural', 
+'Voor wie gelijk goed van start of er gewoon een leuk gitaar bij zoekt, biedt Fender de Classic Design serie. Deze CD-140SCE is een hoogwaardige, doch zeer vriendelijk geprijsde westerngitaar, met ingebouwd elementsysteem. De basis van deze gitaar wordt gevormd door de ovangkol achter- en zijkanten, die zorgen voor een warme klankprojectie. Het bovenblad is van massief sparren (spruce) en geeft de gehele klank extra helderheid mee. De mahonie hals met walnoothouten toets maken dit klankpallet verder compleet, resulterend in een mooi gebalanceerd klinkende gitaar. Ook de topkam mag er wezen, deze is van Graph Tech Nubone. ',
+ 1, 1, 1, 318.00);
 
 insert into products (id, name, description, kleuren_product_id, merk_id, categorie_id, price) values 
 (5, 'Kiss for Corliss, A (Almost a Bride)', 'Nulla ut era.', 1, 1, 1, 14);
@@ -90,3 +92,12 @@ insert into merk (id, brand) values
 --
 insert into categorie (id, category) values
 (1, 'gitaar');
+
+insert into categorie (id, category) values
+(2, 'piano');
+
+insert into categorie (id, category) values
+(3, 'basgitaar');
+
+insert into categorie (id, category) values
+(4, 'drumstel');
